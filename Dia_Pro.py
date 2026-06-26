dc9158e16770800de4affc85a47a8939c7504511
Anmolveer singh <anmolveer08@gmail.com>
2026-06-26 10:12:34 +0530

    done gui again

--- Dia_Pro.py
+++ Dia_Pro.py
@@ -13,3 +13,237 @@ st.set_page_config(
     layout="wide"
 )
 
+# ==========================
+# LOAD MODEL
+# ==========================
+model = joblib.load("random_forest.pkl")
+
+# ==========================
+# BACKGROUND IMAGE
+# ==========================
+
+import base64
+import streamlit as st
+
+def add_bg(image_file):
+    with open(image_file, "rb") as f:
+        encoded = base64.b64encode(f.read()).decode()
+
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/png;base64,{encoded}");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+add_bg("imgback.jpg")   # change to your file name
+st.markdown("""
+<style>
+
+/* Main Title */
+.main-title{
+    text-align:center;
+    font-size:45px;
+    font-weight:bold;
+    color:#003366;
+    text-shadow:2px 2px 4px white;
+    margin-bottom:20px;
+}
+
+/* Section Titles */
+.section-title{
+    text-align:left;
+    font-size:30px;
+    font-weight:bold;
+    color:#FFFFFF;
+    text-shadow:2px 2px 4px black;
+    margin-top:20px;
+}
+
+/* Labels */
+label{
+    color:##00FF7F !important;
+    font-size:18px !important;
+    font-weight:bold !important;
+}
+
+/* Accuracy Text */
+.accuracy{
+    text-align:center;
+    color:#00FF7F;
+    font-size:22px;
+    font-weight:bold;
+}
+
+/* Result Text */
+.result{
+    text-align:center;
+    font-size:28px;
+    font-weight:bold;
+}
+
+</style>
+""", unsafe_allow_html=True)
+
+# ==========================
+# TITLE   '<p class="acc">Random Forest Accuracy : 91.2%</p>',
+# ==========================
+st.markdown(
+    '<div class="main-title">🩺 Dia Predict Pro </div>',
+    unsafe_allow_html=True
+)
+st.markdown(
+    f'<div class="accuracy">{'Random Forest Accuracy'} Accuracy : {91.2:.2f}%</div>',
+    unsafe_allow_html=True
+)
+
+st.divider()
+
+# ==========================
+# INPUTS
+# ==========================
+col1, col2 = st.columns(2)
+
+with col1:
+
+    age = st.number_input("Age", 1, 120, 25)
+
+    gender = st.selectbox(
+        "Gender",
+        ["Female", "Male"]
+    )
+
+    smoking_status = st.selectbox(
+        "Smoking Status",
+        ["never", "former", "current"]
+    )
+
+    bmi = st.number_input("BMI", value=25.0)
+
+    hba1c = st.number_input("HbA1c", value=5.5)
+
+    insulin_level = st.number_input(
+        "Insulin Level",
+        value=15.0
+    )
+
+    heart_rate = st.number_input(
+        "Heart Rate",
+        value=72
+    )
+
+    cholesterol_total = st.number_input(
+        "Cholesterol Total",
+        value=180.0
+    )
+
+    diabetes_risk_score = st.number_input(
+        "Diabetes Risk Score",
+        value=50.0
+    )
+
+with col2:
+
+    alcohol_consumption_per_week = st.number_input(
+        "Alcohol Consumption Per Week",
+        value=0.0
+    )
+
+    physical_activity_minutes_per_week = st.number_input(
+        "Physical Activity Minutes Per Week",
+        value=150
+    )
+
+    diet_score = st.number_input(
+        "Diet Score",
+        value=5.0
+    )
+
+    sleep_hours_per_day = st.number_input(
+        "Sleep Hours Per Day",
+        value=8.0
+    )
+
+    family_history_diabetes = st.selectbox(
+        "Family History Diabetes",
+        [0, 1]
+    )
+
+    hypertension_history = st.selectbox(
+        "Hypertension History",
+        [0, 1]
+    )
+
+    cardiovascular_history = st.selectbox(
+        "Cardiovascular History",
+        [0, 1]
+    )
+
+    diastolic_bp = st.number_input(
+        "Diastolic BP",
+        value=80.0
+    )
+
+    glucose_fasting = st.number_input(
+        "Glucose Fasting",
+        value=90.0
+    )
+
+# ==========================
+# ENCODING
+# ==========================
+gender = 1 if gender == "Male" else 0
+
+smoking_dict = {
+    "never": 0,
+    "former": 1,
+    "current": 2
+}
+
+smoking_status = smoking_dict[smoking_status]
+
+# ==========================
+# PREDICTION
+# ==========================
+if st.button(
+    "🔍 Predict Diabetes Risk",
+    use_container_width=True
+):
+
+    features = np.array([[
+        age,
+        gender,
+        smoking_status,
+        alcohol_consumption_per_week,
+        physical_activity_minutes_per_week,
+        diet_score,
+        sleep_hours_per_day,
+        family_history_diabetes,
+        hypertension_history,
+        cardiovascular_history,
+        bmi,
+        diastolic_bp,
+        heart_rate,
+        cholesterol_total,
+        glucose_fasting,
+        insulin_level,
+        hba1c,
+        diabetes_risk_score
+    ]])
+
+    prediction = model.predict(features)
+
+    st.write("")
+
+    if prediction[0] == 1:
+        st.error("⚠ High Risk of Diabetes")
+    else:
+        st.success("✅ Low Risk of Diabetes")
